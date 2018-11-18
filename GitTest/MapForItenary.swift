4789e8746e39e0bedbe57451bac3470631c82a9a
Jimmy Wright <jimmywright@jimmys-mac-mini.local>
2018-11-18 09:19:05 -0700

    Add new map-for-itenary feature

---
+++ GitTest/MapForItenary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItenary.swift
+//  GitTest
+//
+//  Created by Jimmy Wright on 11/18/18.
+//  Copyright © 2018 Jimmy Wright. All rights reserved.
+//
+
+import UIKit
+
+class MapForItnenary: NSObject {
+  
+}
